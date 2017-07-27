e05e61f57b2a481117ac17ecfc798aaf128e052a
neeil1990 <neeil@mail.ru>
2017-07-27 15:41:03 +0300

    no message

--- bitrix/templates/main/components/bitrix/catalog/catalog/bitrix/catalog.section.list/.default/template.php
+++ bitrix/templates/main/components/bitrix/catalog/catalog/bitrix/catalog.section.list/.default/template.php
@@ -45,7 +45,7 @@ $arCurView = $arViewStyles[$arParams['VIEW_MODE']];
 	<?foreach ($arResult['SECTIONS'] as &$arSection):?>
 
 	<div class="catalog-sections-list__item">
-		<a href="<?=$arSection['SECTION_PAGE_URL']?>" class="link"> <img width="140" alt="<?=$arSection['NAME']?>" src="<?=$arSection['PICTURE']['SRC'];?>" height="120" class="img">
+		<a href="<?=$arSection['SECTION_PAGE_URL']?>" class="link"> <img alt="<?=$arSection['NAME']?>" src="<?=$arSection['PICTURE']['SRC'];?>" height="120" class="img">
 			<span class="name"><?=str_replace(' ','<br>',$arSection['NAME']);?></span></a>
 	</div>
 	<?endforeach;?>
