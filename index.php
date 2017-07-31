274a62bee9b4bb5369370e171919ac8784bd4c5e
neeil1990 <neeil@mail.ru>
2017-07-31 14:08:46 +0300

    no message

--- index.php
+++ index.php
@@ -186,7 +186,7 @@ $APPLICATION->SetTitle("«Полимер» — главная страница 
 				<?
 				$arSelect = Array("ID","IBLOCK_ID", "NAME", "DETAIL_PAGE_URL","PREVIEW_PICTURE","PROPERTY_SALE");
 				$arFilter = array('IBLOCK_ID' => 11, 'ACTIVE' => 'Y','>=TIMESTAMP_X' => date($DB->DateFormatToPHP(CLang::GetDateFormat("SHORT")), strtotime('-30 day')));
-				$res = CIBlockElement::GetList(Array(), $arFilter, false, false, $arSelect);
+				$res = CIBlockElement::GetList(Array(), $arFilter, false, Array("nPageSize" => 30), $arSelect);
 				while($ob = $res->GetNextElement()):
 					$arFields = $ob->GetFields();
 					?>
