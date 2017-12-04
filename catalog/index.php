0de45e7fcb9f63f8e7544c01de98ef8860958bb5
neeil1990 <neeil@mail.ru>
2017-12-04 15:50:00 +0300

    no message

--- catalog/index.php
+++ catalog/index.php
@@ -927,9 +927,9 @@ if($_REQUEST['ELEMENT_SORT_FIELD'] == "CATALOG_AVAILABLE" OR $_REQUEST['ELEMENT_
 		"COMPOSITE_FRAME_TYPE" => "AUTO",
 		"FILE_404" => "",
 		"SEF_URL_TEMPLATES" => array(
-			"sections" => "/catalog/",
-			"section" => "#SECTION_CODE#/",
-			"element" => "#SECTION_CODE#/#ELEMENT_CODE#/",
+			"sections" => "",
+			"section" => "#SECTION_CODE_PATH#/",
+			"element" => "#SECTION_CODE_PATH#/#ELEMENT_CODE#/",
 			"compare" => "compare/",
 			"smart_filter" => "#SECTION_CODE#/filter/#SMART_FILTER_PATH#/apply/",
 		)
