a2915765909df0907eb5c6955cee48aaf943701e
neeil1990 <neeil@mail.ru>
2017-12-05 13:27:25 +0300

    no message

--- bitrix/templates/main/components/bitrix/catalog/catalog/bitrix/catalog.section.list/.default/result_modifier.php
+++ bitrix/templates/main/components/bitrix/catalog/catalog/bitrix/catalog.section.list/.default/result_modifier.php
@@ -101,9 +101,7 @@ foreach($arResult['SECTIONS'] as $key => &$arSection){
 	$arResult['SECTIONS'][$key]['NAME'] = preg_replace('#(~(.*?)~)#is', '', $arSection['NAME']);
 }
 
-if($arResult['SECTION']['ID'] == 0){
-	define("PATH_TO_404", "/404.php");
-	@define("ERROR_404","Y");
-}
+
+
 
 ?>
